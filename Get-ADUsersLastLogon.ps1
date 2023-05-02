0f18307cf9985a0bb1432fc76fc633eba319b002
Alistair McMillan <alistairmcmillan75@gmail.com>
2023-05-02 14:02:57 +0100

    Update Get-ADUsersLastLogon.ps1 to use only spaces for whitespace
    Previously was mix of tabs and spaces.

--- Get-ADUsersLastLogon.ps1
+++ Get-ADUsersLastLogon.ps1
@@ -5,31 +5,30 @@ $TesterAccount = "tester"
 function Get-ADUsersLastLogon() {
     $dcs = Get-ADDomainController -Filter {Name -like "*"}
     $user = Get-ADUser -Identity $TesterAccount
-	$MyArrayList = New-Object -TypeName "System.Collections.ArrayList"
+    $MyArrayList = New-Object -TypeName "System.Collections.ArrayList"
 
     foreach($dc in $dcs) {
-		$time = 0
+        $time = 0
         $hostname = $dc.HostName
-		if (Test-Connection -Computer $hostname -Count 2 -Quiet) {
-			$currentUser = Get-ADUser $user.SamAccountName | Get-ADObject -Server $hostname -Properties lastLogon
-			if($currentUser.LastLogon -gt $time){
-				$time = $currentUser.LastLogon
-				$dt = [DateTime]::FromFileTime($time)
-				$lastLogon = $dt
-			} else {
-				$lastLogon = $time
-			}
-		} else {
-			$lastLogon = "server did not respond"
-		}
-		$row = [PSCustomObject]@{
-								Host = $hostname;
-								User = $user.Name;
-								SAM = $user.SamAccountName
-								LastLogon = $lastLogon
-								}
-		$MyArrayList += $row
-		$row
+        if (Test-Connection -Computer $hostname -Count 2 -Quiet) {
+            $currentUser = Get-ADUser $user.SamAccountName | Get-ADObject -Server $hostname -Properties lastLogon
+            if($currentUser.LastLogon -gt $time){
+                $time = $currentUser.LastLogon
+                $dt = [DateTime]::FromFileTime($time)
+                $lastLogon = $dt
+            } else {
+                $lastLogon = $time
+            }
+        } else {
+            $lastLogon = "server did not respond"
+        }
+        $row = [PSCustomObject]@{
+                                Host = $hostname;
+                                User = $user.Name;
+                                LastLogon = $lastLogon
+                                }
+        $MyArrayList += $row
+        $row
     }
 
     $MyArrayList | Out-GridView
